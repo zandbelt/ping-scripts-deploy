e64403c2685e6f0e381ff0ef454d2140ce348d9d
hans.zandbelt@gmail.com <hans.zandbelt@gmail.com@171b609a-3964-75fa-edf3-e7e307ef598e>
2013-11-15 15:23:31 +0000

    deployment shell scripts for quick and easy deployment of demo and test environments across various versions of PingFederate and IKs

---
+++ trunk/scripts/deploy/pf-deploy-common.sh
@@ -0,0 +1,139 @@
+#!/bin/sh
+###########################################################################
+# Copyright (C) 2013 Ping Identity Corporation
+# All rights reserved.
+#
+# The contents of this file are the property of Ping Identity Corporation.
+# For further information please contact:
+#
+# Ping Identity Corporation
+# 1099 18th St Suite 2950
+# Denver, CO 80202
+# 303.468.2900
+#       http://www.pingidentity.com
+#
+# DISCLAIMER OF WARRANTIES:
+#
+# THE SOFTWARE PROVIDED HEREUNDER IS PROVIDED ON AN "AS IS" BASIS, WITHOUT
+# ANY WARRANTIES OR REPRESENTATIONS EXPRESS, IMPLIED OR STATUTORY; INCLUDING,
+# WITHOUT LIMITATION, WARRANTIES OF QUALITY, PERFORMANCE, NONINFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  NOR ARE THERE ANY
+# WARRANTIES CREATED BY A COURSE OR DEALING, COURSE OF PERFORMANCE OR TRADE
+# USAGE.  FURTHERMORE, THERE ARE NO WARRANTIES THAT THE SOFTWARE WILL MEET
+# YOUR NEEDS OR BE FREE FROM ERRORS, OR THAT THE OPERATION OF THE SOFTWARE
+# WILL BE UNINTERRUPTED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+###########################################################################
+#
+# Author: Hans Zandbelt <hzandbelt@pingidentity.com>
+#
+# Common functions for deploying PingFederate and its Integration Kits.
+#
+##########################################################################
+
+pf_deploy_utility_check() {
+	for f in $* ; do if [ -z `which $f` ] ; then echo " Required utility ${f} is missing: install it first (perhaps from MacPorts)." ; exit ; fi ; done
+}
+
+pf_deploy_unzip() {
+	NAME=$1
+	DESC=$2
+	ZIP=`ls ${NAME}-*.zip`
+	if [ ! -r ${ZIP} ] ; then echo " The $DESC ZIP distribution is missing: download it to this directory first." ; exit ; fi
+	BASE=`basename ${ZIP} .zip`
+	if [ ! -z $3 ] ; then DIR="-d ${BASE}" ; fi
+	echo " [${BASE}] unzip ${ZIP} ... "
+	unzip ${DIR} -q ${ZIP}
+}
+
+pf_deploy_license_check() {
+	if [ ! -r pingfederate.lic ] ; then echo " The PingFederate license file is missing: download it to this directory first." ; exit ; fi
+}
+
+pf_deploy_runsh_jvm_patch() {
+	BASE=$1
+	echo " [${BASE}] patching run.sh for JVM location ... "
+	cat <<EOF | patch -s -p0 ${BASE}/pingfederate/bin/run.sh
+9a10,11
+> JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_07.jdk/Contents/Home
+> 
+EOF
+}
+
+pf_deploy_license_copy() {
+	BASE=$1
+	echo " [${BASE}] copying license file ... "
+	cp pingfederate.lic ${BASE}/pingfederate/server/default/conf
+}
+
+pf_deploy_set_first_login_done() {
+	BASE=$1
+	echo " [${BASE}] set first login done ... "
+	cat > ${BASE}/pingfederate/server/default/data/config-store/com.pingidentity.page.Login.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<con:config xmlns:con="http://www.sourceid.org/2004/05/config">
+    <con:map name="license-map">
+        <con:item name="hasConfiguredServerSettings">true</con:item>
+        <con:item name="key">true</con:item>
+    </con:map>
+</con:config>
+EOF
+}
+
+pf_deploy_set_default_admin_password() {
+	BASE=$1
+	echo " [${BASE}] set default admin password ... "
+	cat > ${BASE}/pingfederate/server/default/data/pingfederate-admin-user.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<adm:administrative-users multi-admin="true" xmlns:adm="http://pingidentity.com/2006/01/admin-users">
+    <adm:user>
+        <adm:user-name>Administrator</adm:user-name>
+        <adm:salt>C0A527949CA5FACEA6D4AD5A6D90868894F53674</adm:salt>
+        <adm:hash>FA947876446F0DF8A123E691A9302C643DBCC91F</adm:hash>
+        <adm:phone-number/>
+        <adm:email-address/>
+        <adm:department/>
+        <adm:description/>
+        <adm:admin-manager>true</adm:admin-manager>
+        <adm:admin>true</adm:admin>
+        <adm:crypto-manager>true</adm:crypto-manager>
+        <adm:auditor>false</adm:auditor>
+        <adm:active>true</adm:active>
+        <adm:password-change-required>false</adm:password-change-required>
+    </adm:user>
+</adm:administrative-users>
+EOF
+}
+
+pf_deploy_launch_macos() {
+	BASE=$1
+	if [ -z $2 ] ; then
+		echo " [${BASE}] launch PingFederate ... "
+		# avoid Mac OS X warning about files downloaded from the Internet
+		xattr -d -r com.apple.quarantine ${BASE}/pingfederate/bin/run.sh
+		# start PingFederate in a new Terminal
+		open -a Terminal ${BASE}/pingfederate/bin/run.sh
+		# wait until PingFederate has been started
+		while [ ! -r ${BASE}/pingfederate/log/server.log ] ; do sleep 1 ; done
+		while [ `tail -n 10 ${BASE}/pingfederate/log/server.log | grep "PingFederate started in"  | wc -l` == 0 ] ; do sleep 1 ; done
+	fi
+}
+
+pf_deploy_pingfederate() {
+	pf_deploy_utility_check unzip
+	if [ -z $1 ] ; then
+		pf_deploy_license_check
+		pf_deploy_unzip pingfederate "PingFederate ZIP"
+		# BASE set now
+		# pf_deploy_runsh_jvm_patch ${BASE}
+		pf_deploy_license_copy ${BASE}
+		pf_deploy_set_first_login_done ${BASE}
+		pf_deploy_set_default_admin_password ${BASE}
+	else
+		BASE=$1
+	fi
+}
---
+++ trunk/scripts/deploy/pf-deploy-java-ik.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+###########################################################################
+# Copyright (C) 2013 Ping Identity Corporation
+# All rights reserved.
+#
+# The contents of this file are the property of Ping Identity Corporation.
+# For further information please contact:
+#
+# Ping Identity Corporation
+# 1099 18th St Suite 2950
+# Denver, CO 80202
+# 303.468.2900
+#       http://www.pingidentity.com
+#
+# DISCLAIMER OF WARRANTIES:
+#
+# THE SOFTWARE PROVIDED HEREUNDER IS PROVIDED ON AN "AS IS" BASIS, WITHOUT
+# ANY WARRANTIES OR REPRESENTATIONS EXPRESS, IMPLIED OR STATUTORY; INCLUDING,
+# WITHOUT LIMITATION, WARRANTIES OF QUALITY, PERFORMANCE, NONINFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  NOR ARE THERE ANY
+# WARRANTIES CREATED BY A COURSE OR DEALING, COURSE OF PERFORMANCE OR TRADE
+# USAGE.  FURTHERMORE, THERE ARE NO WARRANTIES THAT THE SOFTWARE WILL MEET
+# YOUR NEEDS OR BE FREE FROM ERRORS, OR THAT THE OPERATION OF THE SOFTWARE
+# WILL BE UNINTERRUPTED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+###########################################################################
+#
+# Author: Hans Zandbelt <hzandbelt@pingidentity.com>
+#
+# This script deploys PingFederate and the Java Integration Kit.
+#
+# Prerequisites:
+# - The (MacPorts) utility unzip must be installed.
+# - Download into the directory where you run this script from:
+#   a) a pingfederate ZIP distribution (eg. pingfederate-7.1.1.zip)
+#   b) a valid license file (pingfederate.lic)
+#   c) the Java Integration Kit (eg. pf-Java-integration-kit-2.5.1.zip)
+#
+##########################################################################
+
+source "$(dirname "$0")/pf-deploy-common.sh"
+
+pf_deploy_pingfederate $1
+PFBASE=${BASE}
+
+JIK=pf-Java-integration-kit
+pf_deploy_unzip ${JIK} "Java Integration Kit ZIP"
+
+echo " [${BASE}] deploy Java IK JAR files ... "
+cp ${JIK}/dist/*.jar ${PFBASE}/pingfederate/server/default/deploy
+echo " [${BASE}] deploy Java IK sample WARs ... "
+cp -r ${JIK}/sample/*.war ${PFBASE}/pingfederate/server/default/deploy
+echo " [${BASE}] deploy Java IK data.zip ... "
+unzip -q -o ${JIK}/sample/data.zip -d ${PFBASE}/pingfederate/server/default/data
+rm -rf ${JIK}
+
+pf_deploy_launch_macos ${PFBASE} $1
